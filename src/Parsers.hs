5a16210bf712b5890d1000be45bbb10d15199b14
Brian Jing <brianjing404@gmail.com>
2017-11-23 20:02:46 +1100

    Add all commands. Expose test files in .cabal file

--- src/Parsers.hs
+++ src/Parsers.hs
@@ -3,5 +3,8 @@ module Parsers where
 import Models
 
 parseCommand :: String -> Maybe Command
+parseCommand "MOVE" = Just MOVE
 parseCommand "LEFT" = Just LEFT
+parseCommand "RIGHT" = Just RIGHT
 parseCommand "REPORT" = Just REPORT
+parseCommand "PLACE 3,3,EAST" = Just (PLACE (Position 3 3) EAST)
--- test/ParsersSpec.hs
+++ test/ParsersSpec.hs
@@ -8,6 +8,9 @@ import Models
 spec :: Spec
 spec =
   describe "Command parser" $
-    it "should return LEFT" $ do
+    it "should parse all commands" $ do
+      parseCommand "MOVE" `shouldBe` Just MOVE
       parseCommand "LEFT" `shouldBe` Just LEFT
+      parseCommand "RIGHT" `shouldBe` Just RIGHT
       parseCommand "REPORT" `shouldBe` Just REPORT
+      parseCommand "PLACE 3,3,EAST" `shouldBe` Just (PLACE (Position 3 3) EAST)
