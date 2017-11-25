c7f965ad2a8ef32f584ca132fa317c37f8b8ec31
Brian Jing <brianjing404@gmail.com>
2017-11-25 17:08:02 +1100

    Add test that break simulation that reads commands from file. Non-PLACE commands aren't handled properly from the beginner in this mode

--- test/SimulationSpec.hs
+++ test/SimulationSpec.hs
@@ -12,6 +12,8 @@ testCommandsB = ["PLACE 0,0,NORTH", "LEFT"]
 expectedResultB = ValidRobot (Position 0 0) WEST
 testCommandsC = ["PLACE 1,2,EAST", "MOVE", "MOVE", "LEFT", "MOVE"]
 expectedResultC = ValidRobot (Position 3 3) NORTH
+testCommandsThatDontStartWithPlace = ["MOVE", "DIG", "PLACE 1,2,SOUTH", "MOVE"]
+expectedResultNonPlaceStart = ValidRobot (Position 1 1) SOUTH
 testCommandsEmpty = []
 expectedResultEmpty = InvalidRobot
 
@@ -27,6 +29,10 @@ spec = do
       assertEquals (simulateFromFile testFileReaderB "file") initialRobotState expectedResultB
       let testFileReaderC _ = return testCommandsC
       assertEquals (simulateFromFile testFileReaderC "file") initialRobotState expectedResultC
+
+    it "should ignore commands from start of the file until it sees PLACE" $ do
+      let testFileReaderManyMore _ = return testCommandsThatDontStartWithPlace
+      assertEquals (simulateFromFile testFileReaderManyMore "file") initialRobotState expectedResultNonPlaceStart
 
     it "should slack off if file is empty" $ do
       let testFileReaderEmpty _ = return testCommandsEmpty
