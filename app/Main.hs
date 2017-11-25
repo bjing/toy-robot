ef7371fa478f75f3b2b3a7f83f8948ae6f07ea96
Brian Jing <brianjing404@gmail.com>
2017-11-25 16:33:02 +1100

    Now support running commands from file as well as from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Class
 import Control.Monad.IO.Class
 import Control.Monad
+import System.Environment
 import qualified Data.Text as T
 
 import Lib
@@ -13,14 +14,27 @@ import Commands
 import Models
 import qualified Helpers as H
 
-
 main :: IO ()
-main = forever $ do
-  putStrLn "Please start typing commands"
-  runMaybeT $ runStateT simulate initRobotState
+main = do
+  args <- getArgs
+  case args of
+    (inputFilePath:_) -> do
+      _ <- runMaybeT $ runStateT (simulateFromFile inputFilePath) initRobotState
+      return ()
+    [] -> do
+      _ <- runMaybeT $ runStateT simulateFromStdin initRobotState
+      return ()
 
-simulate :: RobotState
-simulate = forever $ do
+simulateFromStdin :: RobotState
+simulateFromStdin = forever $ do
+  liftIO $ putStrLn "Please start typing commands"
   cmdStr <- liftIO getLine
   let cmd = parseCommand (T.pack cmdStr)
   runCommand cmd
+
+simulateFromFile :: FilePath -> RobotState
+simulateFromFile filePath = do
+  content <- liftIO $ readFile filePath
+  let cmdStrs = lines content
+  let cmds = fmap (parseCommand . T.pack) cmdStrs
+  mapM_ runCommand cmds
